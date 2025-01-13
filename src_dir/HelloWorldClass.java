0d631c20278195d5aa3eefdb30bccb682d401972
simon <112705179+simonli357@users.noreply.github.com>
2025-01-13 11:06:01 -0500

    Initial commit

---
+++ src_dir/HelloWorldClass.java
@@ -0,0 +1,10 @@
+public class HelloWorldClass {
+
+//    Main method for the entry of the java program
+    // Access modifier -> private or public
+    // Instance object creation -> static?
+    // Return type -> Is it returning anything or void?
+    // Name of the function -> main or no ?
+    // Method parameters -> their type? command line parameters, an array of strings.
+
+}
